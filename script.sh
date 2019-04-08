446d82f928418c31ea197f06dd072d09080882ea
Alec Missine <alec_missine@yahoo.com>
2019-04-08 18:02:22 -0400

    Move the env fix to script.sh

--- script.sh
+++ script.sh
@@ -8,9 +8,7 @@ URL=https://bin.equinox.io/c/4VmDzA7iaHb/$ZIP
 
 # start ngrok
 export FI1_SUBDOMAIN=stellarfi$(openssl rand -hex 5)
-export FI1_DOMAIN=${FI1_SUBDOMAIN}.ngrok.io
 export FI2_SUBDOMAIN=stellarfi$(openssl rand -hex 5)
-export FI2_DOMAIN=${FI2_SUBDOMAIN}.ngrok.io
 
 cp ngrok.tpl.yml ngrok.yml
 sed -i "s/{NGROK_AUTH_TOKEN}/${NGROK_AUTH_TOKEN}/g" ngrok.yml
@@ -20,6 +18,13 @@ sed -i "s/{FI2_SUBDOMAIN}/${FI2_SUBDOMAIN}/g" ngrok.yml
 ./ngrok start --config ./ngrok.yml --all --log=stdout > ngrok.log &
 
 # build cluster
+export FI1_DOMAIN=${FI1_SUBDOMAIN}.ngrok.io
+export FI2_DOMAIN=${FI2_SUBDOMAIN}.ngrok.io
+export FI1_RECEIVING_SEED=GCNP7JE6KR5CKHMVVFTZJUSP7ALAXWP62SK6IMIY4IF3JCHEZKBJKDZF
+export FI1_SIGNING_SEED=GD4SMSFNFASBHPMCOJAOVYH47OXQM5BGSHFLKHO5BGRGUK6ZOAVDG54B
+export FI2_RECEIVING_SEED=GAAJKG3WQKHWZJ5RGVVZMVV6X3XYU7QUH2YVATQ2KBVR2ZJYLG35Z65A
+export FI2_SIGNING_SEED=GBAPTLS2A72RGEQIK6GQ4F74AIYFS2N7WIQ7LZOYKOJT4KD6MUQEHOEU
+
 docker-compose up &
 
 # start monitoring app
