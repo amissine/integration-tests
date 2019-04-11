2385c8a8713c5ae7ffdacf872e8722d0e59ed1c1
Alec Missine <alec_missine@yahoo.com>
2019-04-11 18:29:32 -0400

    Do not use travis, do not use ngrok

---
+++ local.sh
@@ -0,0 +1,6 @@
+# Run this script when not using travis
+
+export FI1_BRIDGE_VERSION=v0.0.31
+export FI2_BRIDGE_VERSION=v0.0.31
+
+./script.sh
--- script.sh
+++ script.sh
@@ -1,22 +1,23 @@
 # build container image
 docker build ./container -t container
 
-# download ngrok
-ZIP=ngrok-stable-linux-amd64.zip
-URL=https://bin.equinox.io/c/4VmDzA7iaHb/$ZIP
-[ -x ngrok ] || { wget -nv $URL && unzip $ZIP && rm $ZIP; }
-
-# start ngrok
-export FI1_SUBDOMAIN=stellarfi$(openssl rand -hex 5)
-export FI2_SUBDOMAIN=stellarfi$(openssl rand -hex 5)
-export NGROK_AUTH_TOKEN=4yYkPvgmcyEf8JkKLC6m8_7sfHxzF1knmTubyWiNpWa
-
-cp ngrok.tpl.yml ngrok.yml
-sed -i "s/{NGROK_AUTH_TOKEN}/${NGROK_AUTH_TOKEN}/g" ngrok.yml
-sed -i "s/{FI1_SUBDOMAIN}/${FI1_SUBDOMAIN}/g" ngrok.yml
-sed -i "s/{FI2_SUBDOMAIN}/${FI2_SUBDOMAIN}/g" ngrok.yml
-
-./ngrok start --config ./ngrok.yml --all --log=stdout > ngrok.log &
+# No ngrok
+## download ngrok
+#ZIP=ngrok-stable-linux-amd64.zip
+#URL=https://bin.equinox.io/c/4VmDzA7iaHb/$ZIP
+#[ -x ngrok ] || { wget -nv $URL && unzip $ZIP && rm $ZIP; }
+#
+## start ngrok
+#export FI1_SUBDOMAIN=stellarfi$(openssl rand -hex 5)
+#export FI2_SUBDOMAIN=stellarfi$(openssl rand -hex 5)
+#export NGROK_AUTH_TOKEN=4yYkPvgmcyEf8JkKLC6m8_7sfHxzF1knmTubyWiNpWa
+#
+#cp ngrok.tpl.yml ngrok.yml
+#sed -i "s/{NGROK_AUTH_TOKEN}/${NGROK_AUTH_TOKEN}/g" ngrok.yml
+#sed -i "s/{FI1_SUBDOMAIN}/${FI1_SUBDOMAIN}/g" ngrok.yml
+#sed -i "s/{FI2_SUBDOMAIN}/${FI2_SUBDOMAIN}/g" ngrok.yml
+#
+#./ngrok start --config ./ngrok.yml --all --log=stdout > ngrok.log &
 
 # build cluster
 export FI1_DOMAIN=${FI1_SUBDOMAIN}.ngrok.io
